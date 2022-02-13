1a74d9fd4a89b71245ae97d69db49738cd5c58d4
brettrjones-github <contact.waycoolapps@gmail.com>
2022-02-13 12:54:26 -0600

    missing new file

---
+++ AppsSrc/CommonSrc/QtSource/AppletHostChatRoomAdmin.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+// Copyright (C) 2019 Brett R. Jones
+//
+// You may use, copy, modify, merge, publish, distribute, sub-license, and/or sell this software
+// provided this Copyright is not modified or removed and is included all copies or substantial portions of the Software
+//
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+// bjones.engineer@gmail.com
+// http://www.nolimitconnect.com
+//============================================================================
+
+#include "AppletHostChatRoomAdmin.h"
+#include "AppCommon.h"
+#include "AppSettings.h"
+#include "MyIcons.h"
+
+#include <CoreLib/VxDebug.h>
+
+//============================================================================
+AppletHostChatRoomAdmin::AppletHostChatRoomAdmin( AppCommon& app, QWidget * parent )
+: AppletBase( OBJNAME_APPLET_HOST_CHAT_ROOM_ADMIN, app, parent )
+{
+    setAppletType( eAppletHostChatRoomAdmin );
+    ui.setupUi( getContentItemsFrame() );
+    setTitleBarText( DescribeApplet( m_EAppletType ) );
+
+    connect( this, SIGNAL( signalBackButtonClicked() ), this, SLOT( closeApplet() ) );
+
+    m_MyApp.activityStateChange( this, true );
+}
+
+//============================================================================
+AppletHostChatRoomAdmin::~AppletHostChatRoomAdmin()
+{
+    m_MyApp.activityStateChange( this, false );
+}
